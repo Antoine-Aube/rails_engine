43b02f829f467b3f31f869729654df97c46d5f89
Antoine Aube <aaube3@gmail.com>
2023-10-16 20:19:28 -0600

    controller: returns single merchants show

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -2,4 +2,14 @@ class Api::V1::MerchantsController < ApplicationController
   def index
     render json: MerchantSerializer.new(Merchant.all)
   end
+
+  def show
+    render json: MerchantSerializer.new(find_merchant)
+  end
+
+  private
+
+  def find_merchant
+    @merchant = Merchant.find(params[:id])
+  end
 end
