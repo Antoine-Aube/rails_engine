ef20ec56c7c0b29e69405ef92c373fecbc18952a
Antoine Aube <aaube3@gmail.com>
2023-10-16 22:27:14 -0600

    test: request all items

---
+++ spec/requests/api/v1/items_requests_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe 'Items API' do
+  describe 'response for all items' do
+    it "sends a formatted JSON response of all items" do
+      create_list(:item, 10)
+
+      get '/api/v1/items'
+
+      expect(response).to be_successful
+
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+      # require 'pry';binding.pry
+      items = parsed_response[:data]
+      # require 'pry';binding.pry
+      expect(items.count).to eq(10)
+
+      items.each do |item|
+        expect(item).to have_key(:id)
+        expect(item[:id]).to be_an(String)
+
+        expect(item).to have_key(:type)
+        expect(item[:type]).to be_an(String) 
+
+        expect(item).to have_key(:attributes)
+        expect(item[:attributes]).to be_an(Hash)
+      end
+
+      item_attributes = items.first[:attributes]
+      require 'pry';binding.pry
+      expect(item_attributes).to have_key(:name) 
+      expect(item_attributes[:name]).to be_an(String)
+
+      expect(item_attributes).to have_key(:description)
+      expect(item_attributes[:description]).to be_an(String)
+      
+      expect(item_attributes).to have_key(:unit_price) 
+      expect(item_attributes[:unit_price]).to be_an(Float)
+
+      expect(item_attributes).to have_key(:merchant_id)
+      expect(item_attributes[:merchant_id]).to be_an(Integer)
+    end
+  end
+end
