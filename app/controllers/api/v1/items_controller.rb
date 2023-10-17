e02655293871099e19b853be8d371cab5f6c7503
Antoine Aube <aaube3@gmail.com>
2023-10-17 13:03:27 -0600

    controller: show method for single item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,4 +3,14 @@ class Api::V1::ItemsController < ApplicationController
   def index
     render json: ItemSerializer.new(Item.all)
   end
+
+  def show
+    render json: ItemSerializer.new(find_item)
+  end
+
+  private
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
 end
