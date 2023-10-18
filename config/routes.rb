65fc0c7ac97532e225f81dd930a3f193b82061a1
Antoine Aube <aaube3@gmail.com>
2023-10-18 13:31:13 -0600

    routes: update routes for find actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,41 +3,23 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  # namespace :api do 
-  #   namespace :v1 do
-  #     resources :merchants, only: [:index, :show] do 
-
-  #       get 'items', on: :member, to: "merchants_items#index"
-  #     end
-  #   end
-  # end
-
-  # namespace :api do
-  #   namespace :v1 do
-  #     resources :items, only: [:index, :show]
-  #   end
-  # end
-#   get "/api/v1/items/:id/merchant", to: "api/v1/items/merchants#show"
-
-#   namespace :api do
-#     namespace :v1 do
-#       resources :merchants, only: [:index, :show] do
-#         resources :items, module: :merchants, only: [:index]
-#       end
-
-#       resources :items, only: [:index, :show, :create, :update, :destroy] 
-#     end 
-#   end
-# end 
 
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show] do
+        collection do
+          get 'find', to: 'merchants#find_one'
+          get 'find_all', to: 'merchants#find_all'
+        end
         resources :items, module: :merchants, only: [:index]
       end
-
+      
       resources :items, only: [:index, :show, :create, :update, :destroy] do
         get 'merchant', to: 'items/merchants#show', on: :member
+        
+        collection do   
+          get 'find_all', to: 'items#find_all'
+        end 
       end
     end
   end
