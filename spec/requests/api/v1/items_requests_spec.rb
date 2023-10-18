fcbd2bc1a45619cdc3efe5ef568623a7250495f9
Antoine Aube <aaube3@gmail.com>
2023-10-17 18:09:35 -0600

    test: item update tests - full and partial params

--- spec/requests/api/v1/items_requests_spec.rb
+++ spec/requests/api/v1/items_requests_spec.rb
@@ -76,4 +76,43 @@ RSpec.describe 'Items API' do
       expect(item_attributes[:merchant_id]).to be_an(Integer)
     end
   end
+
+  describe "response for updating an item" do 
+    it "sends a formatted JSON response of the updated item" do 
+      merchant = create(:merchant, id: 1)
+      merchant2 = create(:merchant, id: 2)
+      item = create(:item, name: "Old Name", description: "Old Description", unit_price: 1.00, merchant_id: 1)
+      id = item.id
+
+      get "/api/v1/items/#{id}"
+      current_item = JSON.parse(response.body, symbolize_names: true)
+      expect(current_item[:data][:attributes][:name]).to eq("Old Name")
+      expect(current_item[:data][:attributes][:description]).to eq("Old Description")
+
+      patch "/api/v1/items/#{id}", params: {name: "New Name", description: "New Description", unit_price: 2.00, merchant_id: 2}
+
+      updated_item = JSON.parse(response.body, symbolize_names: true)
+      expect(updated_item[:data][:attributes][:name]).to eq("New Name")
+      expect(updated_item[:data][:attributes][:description]).to eq("New Description")
+    end
+
+    it "still updates the item if only one attribute is passed in" do
+        merchant = create(:merchant, id: 1)
+        merchant2 = create(:merchant, id: 2)
+        item = create(:item, name: "Old Name", description: "Old Description", unit_price: 1.00, merchant_id: 1)
+        id = item.id
+  
+        get "/api/v1/items/#{id}"
+        current_item = JSON.parse(response.body, symbolize_names: true)
+        expect(current_item[:data][:attributes][:name]).to eq("Old Name")
+        expect(current_item[:data][:attributes][:description]).to eq("Old Description")
+  
+        patch "/api/v1/items/#{id}", params: {name: "New Name", merchant_id: 2}
+        expect(response).to be_successful
+        expect(response.status).to_not eq(404)
+  
+        updated_item = JSON.parse(response.body, symbolize_names: true)
+        expect(updated_item[:data][:attributes][:name]).to eq("New Name")
+    end
+  end
 end
