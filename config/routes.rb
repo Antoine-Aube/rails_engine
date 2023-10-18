34722e5ad6d8e3b81a943138eaa00204ddba6afd
Antoine Aube <aaube3@gmail.com>
2023-10-17 20:41:25 -0600

    update routes to reflect to RESTful merchants controller for items

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,18 @@ Rails.application.routes.draw do
   #     resources :items, only: [:index, :show]
   #   end
   # end
+#   get "/api/v1/items/:id/merchant", to: "api/v1/items/merchants#show"
+
+#   namespace :api do
+#     namespace :v1 do
+#       resources :merchants, only: [:index, :show] do
+#         resources :items, module: :merchants, only: [:index]
+#       end
+
+#       resources :items, only: [:index, :show, :create, :update, :destroy] 
+#     end 
+#   end
+# end 
 
   namespace :api do
     namespace :v1 do
@@ -24,10 +36,11 @@ Rails.application.routes.draw do
         resources :items, module: :merchants, only: [:index]
       end
 
-      resources :items, only: [:index, :show, :create, :update, :destroy]
+      resources :items, only: [:index, :show, :create, :update, :destroy] do
+        get 'merchant', to: 'items/merchants#show', on: :member
+      end
     end
   end
 end 
-
 
 
